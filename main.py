db35aba2769f586398409de23259d70706ab4cb1
Yehor Sencha <senyehor@gmail.com>
2024-07-10 14:42:05 +0300

    created script entrypoint

---
+++ main.py
@@ -0,0 +1,16 @@
+from aiohttp import ClientSession
+import asyncio
+from scraper import ProductLinksScraper
+
+
+async def get_urls():
+    async with ClientSession() as session:
+        scraper = ProductLinksScraper(session)
+        return await scraper.get_urls()
+
+
+async def main():
+    urls = await get_urls()
+
+if __name__ == '__main__':
+    asyncio.run(main())
