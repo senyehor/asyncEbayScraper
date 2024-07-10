f5e08cc0300200c40f3f3b64023963b4570ed534
Yehor Sencha <senyehor@gmail.com>
2024-07-10 14:45:38 +0300

    renamed scraper.py to product_urls_scraper.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from aiohttp import ClientSession
 import asyncio
-from scraper import ProductLinksScraper
+
+from aiohttp import ClientSession
+
+from product_urls_scraper import ProductLinksScraper
 
 
 async def get_urls():
@@ -11,6 +13,7 @@ async def get_urls():
 
 async def main():
     urls = await get_urls()
+
 
 if __name__ == '__main__':
     asyncio.run(main())
